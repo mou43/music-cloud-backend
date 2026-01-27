9f1443e5bdac14486ec6a6c4e72504b32231eb6c
marta <martaagarciia06@gmail.com>
2026-01-27 01:25:21 +0100

    Primer commit del proyecto: Spring boot + Maven + .gitignore

---
+++ src/main/java/com/tfg/music_cloud_backend/MusicCloudBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tfg.music_cloud_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicCloudBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicCloudBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tfg/music_cloud_backend/MusicCloudBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfg.music_cloud_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicCloudBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
